9f0a798b2ecc7086a0df91398ccfc3ebad6db633
dveloper-Elisa <kwizeraelisa77@gmail.com>
2026-05-02 20:19:36 +0200

    Set tup of the project and initial commit for the mobile app

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import "../global.css"
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-green-700 h-screen justify-center items-center">
+      <Text className="text-white">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
